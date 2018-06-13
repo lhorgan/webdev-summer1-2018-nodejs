eecd5aca936c91fae8964ac964c0ef12a884d026
jannunzi <jannunzi@gmail.com>
2018-06-13 13:39:04 -0400

    added enrollment

--- services/section.service.server.js
+++ services/section.service.server.js
@@ -2,8 +2,13 @@ module.exports = function (app) {
 
   app.post('/api/course/:courseId/section', createSection);
   app.get('/api/course/:courseId/section', findSectionsForCourse)
+  app.post('/api/enroll/section/:sectionId',
+    enrollStudentInSection);
+  app.get('/api/student/section', findSectionsForStudent)
+  app.get('/api/section/:sectionId/student', findStudentsForSection)
 
   var sectionModel = require('../models/section/section.model.server');
+  var enrollmentModel = require('../models/enrollment/enrollment.model.server');
 
   function findSectionsForCourse(req, res) {
     var courseId = req.params['courseId'];
@@ -20,6 +25,40 @@ module.exports = function (app) {
       .createSection(section)
       .then(function (section) {
         res.json(section);
+      })
+  }
+
+  function enrollStudentInSection(req, res) {
+    var user = req.session['currentUser'];
+    var sectionId = req.params.sectionId;
+    var enrollment = {};
+    enrollmentModel
+      .enrollStudentInSection(
+        user._id,
+        sectionId)
+      .then(function (_enrollment) {
+        enrollment = _enrollment;
+        return sectionModel.decrementSectionSeats(sectionId);
+      })
+      .then(function (section) {
+        res.json(enrollment);
+      })
+  }
+
+  function findSectionsForStudent(req, res) {
+    var user = req.session['currentUser'];
+    enrollmentModel
+      .findSectionsForStudent(user._id)
+      .then(function (enrollments) {
+        res.json(enrollments);
+      })
+  }
+
+  function findStudentsForSection(req, res) {
+    enrollmentModel
+      .findStudentsForSection(req.params.sectionId)
+      .then(function (enrollments) {
+        res.json(enrollments);
       })
   }
 };
